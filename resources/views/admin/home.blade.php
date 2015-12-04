417d9327c401046c002680fe464bf428bb8393c7
Asael Chávez <asaelx@gmail.com>
2015-12-04 00:53:50 -0600

    Sidebar basic styling

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="{{ asset('css/all.css') }}">
+    <link rel="stylesheet" href="{{ asset('bower/typicons.font/src/font/typicons.min.css') }}">
     <title>Admin</title>
   </head>
   <body>
@@ -10,19 +11,20 @@
       <div class="profile">
         <div class="photo"><img src="https://s3.amazonaws.com/uifaces/faces/twitter/allisongrayce/48.jpg" class="img"></div>
         <div class="title">Personal Blog</div>
-        <div class="name">Brenda Graham</div>
+        <div class="name">Brenda Graham</div><span class="typcn typcn-arrow-sorted-down"></span>
       </div>
-      <div class="search">
-        <input type="search" name="s">
+      <div class="search"><span class="typcn typcn-zoom"></span>
+        <input type="search" name="s" placeholder="Buscar...">
       </div>
       <nav class="nav">
         <ul class="options">
-          <li class="option"><a href="#" class="link">New Post</a></li>
-          <li class="option"><a href="#" class="link">Content</a></li>
-          <li class="option"><a href="#" class="link">Team</a></li>
-          <li class="option"><a href="#" class="link">Settings</a></li>
+          <li class="option"><a href="#" class="link"> <span class="typcn typcn-pen"></span>New Post</a></li>
+          <li class="option"><a href="#" class="link"> <span class="typcn typcn-document-text"></span>Content</a></li>
+          <li class="option"><a href="#" class="link"> <span class="typcn typcn-user"></span>Team</a></li>
+          <li class="option"><a href="#" class="link"> <span class="typcn typcn-cog"></span>Settings</a></li>
         </ul>
       </nav>
     </aside>
+    <div class="editor">Editor</div>
   </body>
 </html>
